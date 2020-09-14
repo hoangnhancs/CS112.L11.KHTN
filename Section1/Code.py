7f44c4c7e803c93cce529446df62261f5b42227a
Son Luu <sonlh.cs.uit@gmail.com>
2020-09-14 11:39:33 +0700

    remove BOT, move code from BOT.py to Code.py

---
+++ Section1/Code.py
@@ -0,0 +1,26 @@
+def kadane(seqs):
+  max_sum_present = 0
+  max_ending_at = 0
+  start_index = 0
+  start_temp = -1
+  ending_index = 0
+  for i in range(len(seqs)):
+    max_ending_at += seqs[i]
+    if (max_ending_at < 0):
+      max_ending_at = 0
+      start_temp = -1
+    else:
+      if start_temp == -1:
+        start_temp = i
+      if max_sum_present < max_ending_at:
+        max_sum_present = max_ending_at
+        start_index = start_temp
+        ending_index = i
+  return start_index+1, ending_index+1, max_sum_present 
+
+#a = [2, -4, 5, -8, 4, -1, -1, 1, 1, 1, -2, 2, 4, -6, 9, -4]
+n = input()
+n = int(n)
+a = list(map(int, input().split()))
+start,stop,profit = kadane(a)
+print(start,stop,profit)
