fd1124785da48379d5015c1e6b790ddf9ceb1bb8
Thai Hoang Nhan <thaihoangnhantk17lqd@gmail.com>
2020-09-15 20:45:00 +0700

    upload project4.py

---
+++ EulerProjects/project4.py
@@ -0,0 +1,15 @@
+def reversed_string_to_string(string_convert, seperator = ''):
+    #convert string to a revesed list by reversed() function
+    return seperator.join(reversed(string_convert))
+    #it is a base function, and I write it for reference purposes only
+
+
+
+max = 0
+for i in range (999, 99, -1):
+    for j in range (999, 99, -1):
+        if str(i*j)==str(i*j)[::-1]:
+            if(max<i*j):
+                max=i*j
+print(max)
+
