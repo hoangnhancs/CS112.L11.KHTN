d9734b6ee3d8e0bf641e2519ac93431ea05c578e
Thai Hoang Nhan <thaihoangnhantk17lqd@gmail.com>
2020-09-15 20:47:41 +0700

    update project4.py

--- EulerProjects/project4.py
+++ EulerProjects/project4.py
@@ -3,10 +3,10 @@ def reversed_string_to_string(string_convert, seperator = ''):
     return seperator.join(reversed(string_convert))
     #it is a base function, and I write it for reference purposes only
 
-
+#palindromic number always divisible by 11
 
 max = 0
-for i in range (999, 99, -1):
+for i in range (990, 109, -11):
     for j in range (999, 99, -1):
         if str(i*j)==str(i*j)[::-1]:
             if(max<i*j):
