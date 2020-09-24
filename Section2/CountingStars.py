e505a2f084f64379fb150f3b4d65e2a268603fe0
Son Luu <sonlh.cs.uit@gmail.com>
2020-09-24 13:01:18 +0700

    fix report

--- Section2/CountingStars.py
+++ Section2/CountingStars.py
@@ -1,44 +1,33 @@
+import time
+# nhập 
 # n = int(input())
 # a, b, c, d = map(int,input().replace('/',' ').split())
+n = int(input())
+a, b, c, d = map(int,input().replace('/',' ').split())
 
-# def get_points(n,a,b,c,d):
+points = [map(int,input().split()) for _ in range(n)]
+rpoints = [(-a*x+b*y,-(-c*x+d*y)) for x,y in points]
+spoints = sorted([(x,y) for x,y in rpoints if x>0 and y>0], key=lambda x: (x[0], -x[1]))
+new_points = spoints
+
+
+# def get_points(n):
 #     points = []
-#     for i in range(0,n):
-#         x,y = map(int,input().split()[:2])
-#         points.append((x,y))
+#     points = [map(int,input().split()) for _ in range(n)]
+#     # for i in range(0,n):
+#     #     x,y = map(int,input().split()[:2])
+#     #     points.append((x,y))
 #     return points
-        
+# # hết phần nhập, tới phần ánh xạ x, y thành x,y mới         
 # def pre_process(points):
 #     rpoints = list((-a*x+b*y,-(-c*x+d*y)) for x,y in points)
 #     spoints = sorted([(x,y) for x,y in rpoints if x>0 and y>0], key=lambda x: (x[0], -x[1]))
 #     return spoints
 
     
-# points = get_points(n,a,b,c,d)
-# a = pre_process(points)
-
-
-# print(a)
-import time
-n = int(input())
-a, b, c, d = map(int,input().replace('/',' ').split())
-
-def get_points(n,a,b,c,d):
-    points = []
-    for i in range(0,n):
-        x,y = map(int,input().split()[:2])
-        points.append((x,y))
-    return points
-        
-def pre_process(points):
-    rpoints = list((-a*x+b*y,-(-c*x+d*y)) for x,y in points)
-    spoints = sorted([(x,y) for x,y in rpoints if x>0 and y>0], key=lambda x: (x[0], -x[1]))
-    return spoints
-
-    
-points = get_points(n,a,b,c,d)
-points_ = pre_process(points)
-    
+# points = get_points(n)
+# points_ = pre_process(points)
+# dãy con tăng 
 def bin_search(A, l, r, key): 
     while (r - l > 1): 
       
@@ -85,100 +74,27 @@ def LIS(A, size):
   
 
 # array = [10 , 22 , 9 , 33 , 21 , 50 , 41 , 60] 
-array = list(map(lambda x:x[1],points_))
-start = time.time()
+array = list(map(lambda x:x[1],new_points))
+# start = time.time()
 ans = LIS(array,len(array))
-end = time.time()
+# end = time.time()
 print(ans)
-print(end - start)
+# print(end - start)
 
-
-
-
-
-
-# # from bisect import *
-
-# n = int(input())
-# a, b, c, d = map(int,input().replace('/',' ').split())
-
-# points = [map(int,input().split()) for _ in range(n)]
-# rpoints = [(-a*x+b*y,-(-c*x+d*y)) for x,y in points]
-# print(rpoints)
-# spoints = sorted([(x,y) for x,y in rpoints if x>0 and y>0], key=lambda x: (x[0], -x[1]))
-# print(spoints)
-# f = []
-# for x,y in spoints:
-#     print(f)
-#     i = bisect_left(f,y)
-#     if i == len(f):
-#         f.append(y)
-#     else:
-#         f[i] = y
-# print(f)
-# print(len(f))
-
-# n = int(input())
-# a, b, c, d = map(int,input().replace('/',' ').split())
-
-# def get_points(n,a,b,c,d):
-#     points = []
-#     for i in range(0,n):
-#         x,y = map(int,input().split()[:2])
-#         points.append((x,y))
-#     return points
-        
-# def my_key(point):
-#     x = point[0]
-#     y = point[1]
-#     return (c*x - d*y,a*x - b*y)
-
-# points = get_points(n,a,b,c,d)
-# points.sort(key=my_key)
-# print(points)
-
-# -------------------
-
-
-
-# 
-# n = int(input())
-# a,c = input().split()[:2]
-# a,b = a.split('/')
-# c,d = c.split('/')
-
-# a=int(a)
-# b=int(b)
-# c=int(c)
-# d=int(d)
-
-# points = []
-
-# for i in range(1,n+1):
-# 	x,y = map(int,input().split()[:2])
-# 	if b*y-a*x > 0 and d*y-c*x < 0:
-# 		points.append((x,y))
-
-# def mykey(x):
-# 	return (c*x[0] - d*x[1],a*x[0] - b*x[1])
-# print(points)
-# points.sort(key=mykey)
-# print(points)
-# lis = []
-# for point in points:
-# 	low = 0
-# 	high = len(lis) - 1
-# 	ret = 0
-# 	while low <= high:
-# 		mid = (low + high)//2
-# 		if lis[mid] < b*point[1] - a*point[0]:
-# 			ret = mid + 1
-# 			low = mid + 1
-# 		else:
-# 			high = mid - 1
-# 	if ret == len(lis):
-# 		lis.append(b*point[1] - a*point[0])
-# 	else:
-# 		lis[ret] = b*point[1] - a*point[0]
-# print(lis)
-# print(len(lis))
+# 15
+# 1/4 2/1
+# 3 1
+# 6 2
+# 9 3
+# 12 4
+# 15 5
+# 2 1
+# 4 2
+# 5 3
+# 7 4
+# 1 3
+# 3 4
+# 2 5
+# 4 5
+# 1 6
+# 6 6
