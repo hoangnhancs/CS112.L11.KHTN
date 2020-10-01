aa97527b9dd3ab509fe9c89142d34f37692c470a
Son Luu <sonlh.cs.uit@gmail.com>
2020-10-01 12:33:55 +0700

    fix sec4

--- Section4/doimoi.py
+++ Section4/doimoi.py
@@ -1,11 +1,18 @@
-# a,k,b,m,n = map(int, input().split())
 a, k, b, m, n = map(int, input().split())
- 
-sum=0
-#result = int(n/(a+b))
-result = int(n//((1-1/k)*a+(1-1/m)*b)) - 2
-while(sum<n):
-    sum = (result - int(result//k))*a + (result - int(result//m))*b
-    result+=1
+sum = 0
 
-print(result-1)
+def f(x):
+    """
+    :param x: số ngày 2 đội đã chặt cây
+    :return: số cây 2 đội đã chặt trong x ngày
+    """
+    return (x - int(x // k)) * a + (x - int(x // m)) * b
+
+result = int(n // ((1 - 1 / k) * a + (1 - 1 / m) * b)) - 1  # giải nghiệm
+
+while (sum < n):
+    sum = f(result)
+
+    result += 1
+
+print(result - 1)
